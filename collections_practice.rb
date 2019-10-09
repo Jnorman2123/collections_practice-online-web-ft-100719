2500638d5c96368f188f65eca82cad37fe23e863
Jonathan Norman <jnorman2123@gmail.com>
2019-10-09 19:23:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,9 +11,5 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  item2 = array[1]
-  item3 = array[2]
-  item2 = array[2]
-  item3 = array[1]
-  array
+  array[1], array[2] = array[2], array[1]
 end
